c11d4d0d7143ba6383f9408c150c96ac632501c2
agustin <agustin@agustin.(none)>
2013-10-10 16:05:41 -0300

    first commit

---
+++ Color.java
@@ -0,0 +1,8 @@
+
+public enum Color {
+   ROJO(1), VIOLETA(2), AZUL(3), AMARRILO(4) , NEGRO(5), VERDE(6) ,MARRON(7),BLANCO(-1);
+   int color;
+   Color(int color){
+	   this.color = color;
+   }
+}
---
+++ Dot.java
@@ -0,0 +1,9 @@
+
+public class Dot {
+	private Position start;
+	private Position end;
+	public Dot(Position a , Position b){
+		this.start=a;
+		this.end=b;
+	}
+}
