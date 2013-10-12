c50c2e5ac8df740eb0147005771de18011799db7
agustin <agustin@agustin.(none)>
2013-10-12 18:56:51 -0300

    parseo + pseudo test

--- src/parser.java
+++ src/parser.java
@@ -19,6 +19,7 @@ public class parser {
 			buffer = new BufferedReader(new FileReader(archivo));
 			while ((line = (buffer.readLine())) != null) {
 				numbers.add(line);
+				System.out.println(line);
 			}
 			return numbers;
 		}
@@ -32,39 +33,40 @@ public class parser {
 	/*trata de armar la matrix con los colores y blancos , sino puede retorna null*/
 	public Board setearJuego(ArrayList<String> nivel) {
 		int matrix[][]=makeBoard(nivel.get(0));
+		boolean flag=false;
+		System.out.println(matrix[0].length);
 		ArrayList<Dot>listcolor=new ArrayList<Dot>();
 		Color colors[]={Color.RED, Color.MAGENTA,Color.BLUE,Color.YELLOW ,
 			    Color.BLACK,Color. GREEN ,Color. PINK, Color.GRAY};
-		for(int i=1;i<nivel.size();i++){
-			String aux=nivel.get(i);
-			for(int c=0;c<matrix.length;c++){
-				char character=aux.charAt(c);
-					for(int j=0;j<colors.length;j++)
-					{
-						if(colors[j].color==(int)character){
-							matrix[i][c]=(int)character;
-							if(!checkColorQty(listcolor,colors[j],i,c))
-								return null;
-						}
-						else
-						{
-							if(character==' '){
+		for(int i=1;i<matrix[0].length;i++){
+				String aux=nivel.get(i);
+				for(int c=0;c<matrix.length;c++){
+				
+						char character=aux.charAt(c);
+						if(character==' ')
+						{	System.out.println("leo espacios");
 								matrix[i][c]=Color.WHITE.color;
-							}
-							else
-							{
-								return null;
-							}
 						}
-						
-			}	
-			
+						else{
+								int num=character-'0';
+								System.out.println("numero"+num);
+								if(!(num<=9)&&(num>=0))
+									return null;
+								for(int j=0;j<colors.length;j++)
+								{	
+									if(colors[j].color==num){
+										matrix[i][c]=colors[j].color;
+										if(!checkColorQty(listcolor,colors[j],i,c)){
+											return null;
+										}
+											
+									}
+								}
+						}
+				}
 		}
-		
+		return new Board(matrix, listcolor);
 	}
-		return new Board(matrix,listcolor);
-	}
-
 /*chequea y guarda la posicion de los colores actuales si hay mas de 2 colores del mismo tipo retorna false*/
 	private boolean checkColorQty(List<Dot> listcolor, Color color,
 			int i, int c) {
@@ -92,14 +94,29 @@ public class parser {
 		int i=0;
 		char a=string.charAt(i);
 		i++;
-		if(string.charAt(i)!=',')
+		if(string.charAt(i)!=','){
 			return null;
-		i++;
+		}
+			i++;
 		char b=string.charAt(i);
-		
+	
 		if(a==b)
-			return new int[a][b];
+		{
+			return new int[a-'0'][b-'0'];
+		}
+		
 		return null;
+			
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		parser p=new parser();
+		Board b;
+		File f= new File("/home/agustin/TPE/adetp/levels/level1.txt");
+		ArrayList<String>nivel=p.levantarNivel(f);
+		b=p.setearJuego(nivel);
+		System.out.println(b.colsSize());
 	}
 }
 			
