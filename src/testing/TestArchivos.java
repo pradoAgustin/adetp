09ae98079d9bcc8c71d1436be8c3c6fb5f90692f
fernandoencaballito <fernando.en.caballito@gmail.com>
2013-10-12 18:55:53 +0000

    a

--- src/testing/TestArchivos.java
+++ src/testing/TestArchivos.java
@@ -3,6 +3,7 @@ package testing;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.junit.BeforeClass;
@@ -12,18 +13,14 @@ import backend.Parser;
 
 public class TestArchivos {
 	static Parser parser;
-	 @BeforeClass
-	  public static void testSetup() {
-		 
-		 Parser parser=new Parser();
-	  }
+	
 	
 	/*
 	 * se testea la lectura del archivo del enunciado
 	 */
 	@Test
 	public void lecturaArchivoValido() throws IOException{
-		
+		Parser parser=new Parser();
 		Object aux=parser.levantarNivel("ArchivosEntrada"+File.separator+"ArchivoEnunciado.txt");
 		assertTrue(aux!=null);
 	}
@@ -48,6 +45,51 @@ public class TestArchivos {
 		
 		
 		parser.levantarNivel("ArchivosEntrada"+File.separator+"ArchivoInvalido2.txt");
+	}
+	
+
+	/*se trata de leer el archivo invalido3, el cual tiene menos filas delas que especifica en la primera línea
+	 * 
+	 */
+	@Test(expected = IOException.class)
+	public void lecturaArchivoNoValido3() throws Exception{
+		
+		
+		parser.levantarNivel("ArchivosEntrada"+File.separator+"ArchivoInvalido3.txt");
+	}
+	
+
+	/*se trata de leer el archivo invalido4, el cual tiene una columna menos en la segunda línea
+	 * 
+	 */
+	@Test(expected = IOException.class)
+	public void lecturaArchivoNoValido4() throws Exception{
+		
+		
+		parser.levantarNivel("ArchivosEntrada"+File.separator+"ArchivoInvalido4.txt");
+	}
+	
+	
+	
+	/*se trata de leer el archivo invalido5, el cual tiene un color sin destino
+	 * 
+	 */
+	@Test(expected = IOException.class)
+	public void lecturaArchivoNoValido5() throws Exception{
+		
+		
+		parser.levantarNivel("ArchivosEntrada"+File.separator+"ArchivoInvalido5.txt");
+	}
+	
+
+	/*se trata de leer  un archivo que no existe
+	 * 
+	 */
+	@Test(expected = FileNotFoundException.class)
+	public void lecturaArchivoNoValido6() throws Exception{
+		
+		
+		parser.levantarNivel("ArchivosEntrada"+File.separator+"inexistente.txt");
 	}
 	
 	
